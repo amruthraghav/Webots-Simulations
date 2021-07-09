572c83678a41555feebc31058c3957e3a6c2cef2
amruthraghav <amruth@hku.hk>
2021-07-09 22:07:39 +0800

    "Final Code"

--- Robotis_OP2_Walk.py
+++ Robotis_OP2_Walk.py
@@ -17,61 +17,51 @@ except ImportError:
 
 class Walk():
     def __init__(self):
-        self.robot = Robot()  
-        self.mTimeStep = int(self.robot.getBasicTimeStep()) 
-        self.HeadLed = self.robot.getLED('HeadLed')  
-        self.EyeLed = self.robot.getLED('EyeLed')  
-        self.HeadLed.set(0xff0000)  
-        self.EyeLed.set(0xa0a0ff)  
-        self.mAccelerometer = self.robot.getAccelerometer('Accelerometer')  
-        self.mAccelerometer.enable(self.mTimeStep)
+        self.robot = Robot()  # Initialize the Robot class to control the robot 
+        self.mTimeStep = int(self.robot.getBasicTimeStep()) # Get the current simulation time mTimeStep of each simulation step
+        self.HeadLed = self.robot.getLED('HeadLed')   # Get head LED light
+        self.EyeLed = self.robot.getLED('EyeLed')   # Get head LED light
+        self.HeadLed.set(0xff0000)  # Turn on the head LED light and set a color
+        self.EyeLed.set(0xa0a0ff)  # Light up the eye LED lights and set a color
+        self.mAccelerometer = self.robot.getAccelerometer('Accelerometer')  # Get the accelerometer
+        self.mAccelerometer.enable(self.mTimeStep)# Activate the sensor and update the value with mTimeStep as the cycle
 
         self.fup = 0
-        self.fdown = 0
+        self.fdown = 0 # Define two class variables for later judging whether the robot has fallen down
         
-        self.mGyro = self.robot.getGyro('Gyro')  
-        self.mGyro.enable(self.mTimeStep)  
+        self.mGyro = self.robot.getGyro('Gyro')  # Get the gyroscope
+        self.mGyro.enable(self.mTimeStep)  # Activate the gyroscope and update the value with mTimeStep as the cycle
 
-        self.camera = self.robot.getCamera('Camera')
-        self.camera.enable(self.mTimeStep)
+        self.camera = self.robot.getCamera('Camera') # Get the Camera
+        self.camera.enable(self.mTimeStep)# Activate the Camera and update the value with mTimeStep as the cycle
         
-        self.accelerometer = self.robot.getDevice('Accelerometer')
-        self.accelerometer.enable(self.mTimeStep)
+        self.accelerometer = self.robot.getDevice('Accelerometer') # Get the Accelerometer
+        self.accelerometer.enable(self.mTimeStep)# Activate the Accelerometer and update the value with mTimeStep as the cycle
         
-        self.gps = self.robot.getGPS('gps')
-        self.gps.enable(self.mTimeStep)
-        initialgps = self.gps.getValues()
+        self.gps = self.robot.getGPS('gps')# Get the GPS
+        self.gps.enable(self.mTimeStep)# Activate the GPS and update the value with mTimeStep as the cycle
         
-        self.left_touch = self.robot.getTouchSensor('left_touch')
-        self.left_touch.enable(self.mTimeStep)
         
-        self.right_touch = self.robot.getTouchSensor('right_touch')
-        self.right_touch.enable(self.mTimeStep)
-        
-
-        self.positionSensors = []
-        
+        self.positionSensors = [] # Initialize the joint angle sensor
         self.positionSensorNames = ('ShoulderR', 'ShoulderL', 'ArmUpperR', 'ArmUpperL',
                                     'ArmLowerR', 'ArmLowerL', 'PelvYR', 'PelvYL',
                                     'PelvR', 'PelvL', 'LegUpperR', 'LegUpperL',
                                     'LegLowerR', 'LegLowerL', 'AnkleR', 'AnkleL',
-                                    'FootR', 'FootL', 'Neck', 'Head')  
+                                    'FootR', 'FootL', 'Neck', 'Head')  # Initialize each sensor name
 
-        
+        # Acquire and activate each sensor, update the value with mTimeStep as the cycle
         for i in range(0, len(self.positionSensorNames)):
             self.positionSensors.append(self.robot.getPositionSensor(self.positionSensorNames[i] + 'S'))
             self.positionSensors[i].enable(self.mTimeStep)
-            #self.device[self.positionSensorNames[i]] = self.robot.getMotor(self.positionSensorNames[i])
+            
 
-        self.mKeyboard = self.robot.getKeyboard()  
-        self.mKeyboard.enable(self.mTimeStep)  
+        self.mKeyboard = self.robot.getKeyboard()  # Initialize keyboard reading class
+        self.mKeyboard.enable(self.mTimeStep)   # Read from the keyboard with mTimeStep as the cycle
 
-        self.mMotionManager = RobotisOp2MotionManager(self.robot) 
-        self.mGaitManager = RobotisOp2GaitManager(self.robot, "config.ini") 
+        self.mMotionManager = RobotisOp2MotionManager(self.robot) # Initialize the robot action group controller
+        self.mGaitManager = RobotisOp2GaitManager(self.robot, "config.ini") # Initialize the robot gait controller
         
         
-        
-    
     def myStep(self):
         ret = self.robot.step(self.mTimeStep)
         if ret == -1:
@@ -90,139 +80,87 @@ class Walk():
         print("Press the space bar to start/stop walking")
         print("Use the arrow keys to move the robot while walking")
         
-        self.myStep()
+        self.myStep() # Simulate a step size and refresh the sensor reading
 
-        self.mMotionManager.playPage(9)  
-        self.wait(200)  
+        self.mMotionManager.playPage(9)  #Perform the 9th action of the action group,initialize the standing posture, and prepare to walk
+        self.wait(200)  # Wait 200ms
 
-        self.isWalking = False
-        initialgps = self.gps.getValues()
-        print("Starting Location:",initialgps)
+        self.isWalking = False # The robot does not enter the walking state at the beginning
 
-        print("Left Touch:", self.left_touch.getValue())
+        initialgps = self.gps.getValues() # store the starting GPS Location of the Robot in X,Y,Z Axis
+        print("Starting Location:",initialgps) # printing the statement 
+
+       
 
         while True:
-            self.checkIfFallen()
-            self.mGaitManager.setXAmplitude(0.0)  
-            self.mGaitManager.setAAmplitude(0.0) 
-            key = 0  
-            key = self.mKeyboard.getKey() 
-            if key == 32:  
-                if (self.isWalking):  
+            
+            self.mGaitManager.setXAmplitude(0.0)  # Advance to 0
+            self.mGaitManager.setAAmplitude(0.0) # Turn to 0
+            key = 0  # The initial keyboard reading defaults to 0
+            key = self.mKeyboard.getKey() # Read input from keyboard
+            if key == 32:  # If a space is read, change the walking state
+                if (self.isWalking):  # If the current robot is walking, stop the robot
                     self.mGaitManager.stop()
                     self.isWalking = False
                     self.wait(200)
-                else:  
+                else:  # If the robot is currently stopped, it will start walking
                     self.mGaitManager.start()
                     self.isWalking = True
                     self.wait(200)
-            elif key == 315:
+            elif key == 315: # 
                 self.mGaitManager.setXAmplitude(1.0)
-                #print("Accelerometer:",self.accelerometer.getValues())
                 print("GPS:",self.gps.getValues())
-                print("Total Distance traveled in Terms of X Coordinate:",self.gps.getValues()[0]-initialgps[0])
+                print("Total Distance traveled in Terms of X Coordinate:",abs(self.gps.getValues()[0]-initialgps[0]))
                 #print("GPS Y:",self.gps.getValues()[1]-initialgps[1])
-                print("Total Distance traveled in Terms of Z Coordinate::",self.gps.getValues()[2]-initialgps[2])
+                print("Total Distance traveled in Terms of Z Coordinate::",abs(self.gps.getValues()[2]-initialgps[2]))
                 print("Speed:",self.gps.getSpeed())
-                #print("Left Touch:", self.left_touch.getValue())
+               
             elif key == 317:  
                 self.mGaitManager.setXAmplitude(-1.0)
-                print("Accelerometer:",self.accelerometer.getValues())
                 print("GPS:",self.gps.getValues())
-                print("Total Distance traveled in Terms of X Coordinate:",self.gps.getValues()[0]-initialgps[0])
+                print("Total Distance traveled in Terms of X Coordinate:",abs(self.gps.getValues()[0]-initialgps[0]))
                 #print("GPS Y:",self.gps.getValues()[1]-initialgps[1])
-                print("Total Distance traveled in Terms of Z Coordinate::",self.gps.getValues()[2]-initialgps[2])
+                print("Total Distance traveled in Terms of Z Coordinate::",abs(self.gps.getValues()[2]-initialgps[2]))
                 print("Speed:",self.gps.getSpeed())
                 
             elif key == 316: 
                 self.mGaitManager.setAAmplitude(-0.5)
-                print("Accelerometer:",self.accelerometer.getValues())
                 print("GPS:",self.gps.getValues())
-                print("Total Distance traveled in Terms of X Coordinate:",self.gps.getValues()[0]-initialgps[0])
+                print("Total Distance traveled in Terms of X Coordinate:",abs(self.gps.getValues()[0]-initialgps[0]))
                 #print("GPS Y:",self.gps.getValues()[1]-initialgps[1])
-                print("Total Distance traveled in Terms of Z Coordinate::",self.gps.getValues()[2]-initialgps[2])
+                print("Total Distance traveled in Terms of Z Coordinate::",abs(self.gps.getValues()[2]-initialgps[2]))
                 print("Speed:",self.gps.getSpeed())
                 
             elif key == 314: 
-                self.mGaitManager.setAAmplitude(0.5) 
-                print("Accelerometer:",self.accelerometer.getValues())
+                self.mGaitManager.setAAmplitude(0.5)
                 print("GPS:",self.gps.getValues())
-                print("Total Distance traveled in Terms of X Coordinate:",self.gps.getValues()[0]-initialgps[0])
+                print("Total Distance traveled in Terms of X Coordinate:",abs(self.gps.getValues()[0]-initialgps[0]))
                 #print("GPS Y:",self.gps.getValues()[1]-initialgps[1])
-                print("Total Distance traveled in Terms of Z Coordinate::",self.gps.getValues()[2]-initialgps[2])
+                print("Total Distance traveled in Terms of Z Coordinate::",abs(self.gps.getValues()[2]-initialgps[2]))
                 print("Speed:",self.gps.getSpeed())
                 
-            self.mGaitManager.step(self.mTimeStep)  
-            self.myStep()
-            
+            self.mGaitManager.step(self.mTimeStep)  # The gait generator generates a step-length movement
+            self.myStep() # Simulate a step size
 
-    def checkIfFallen(self):
-        acc = self.mAccelerometer.getValues()
-        acc_tolerance = 80.0
-        acc_step = 100
-        # print(acc)
-        # count how many steps the accelerometer
-        # says that the robot is down
-        if (acc[1] < 512.0 - acc_tolerance):
-            self.fup = self.fup + 1
-        else:
-            self.fup = 0
-
-        if (acc[1] > 512.0 + acc_tolerance):
-            self.fdown = self.fdown + 1
-        else:
-            self.fdown = 0
-            
-        if (acc[0] < 512.0 - acc_tolerance):
-            self.fright = self.fright + 1
-        else:
-            self.fright = 0
-
-        if (acc[0] > 512.0 + acc_tolerance):
-            self.fleft = self.fleft + 1
-        else:
-            self.fleft = 0
-
-        # the robot face is down
-        if (self.fup > acc_step):
-            print("1")
-            self.mMotionManager.playPage(10)  # f_up
-            self.mMotionManager.playPage(9)   # init position
-            self.fup = 0
-  
-        # the back face is down
-        elif (self.fdown > acc_step) :
-            print("2")
-            self.mMotionManager.playPage(11)  # b_up
-            self.mMotionManager.playPage(9)   # init position
-            self.fdown = 0
-            
-        # the back face is down
-        elif (self.fright > acc_step) :
-            print("3")
-            self.mMotionManager.playPage(13)
-            self.fright = 0
-            
-        # the back face is down
-        elif (self.fleft > acc_step) :
-            print("4")
-            self.mMotionManager.playPage(12)
-            self.fleft  = 0
-        #self.mMotionManager.playPage(10) 
-        #self.mMotionManager.playPage(11) 
-        #self.mMotionManager.playPage(9)
     
     def auto_run(self):
         self.myStep()  # Simulate a step to refresh the sensor reading 
         self.mMotionManager.playPage(9)  # Perform action group 9, initialize standing position, and prepare to walk  
         self.wait(200)  # Wait 200ms  
         self.isWalking = False  # Initially, the robot did not enter the walking state  
+        initialgps = self.gps.getValues() # store the starting GPS Location of the Robot in X,Y,Z Axis
+        print("Starting Location:",initialgps) # printing the statement 
         while True:
-            self.checkIfFallen()  # Determine if fall or not
+            
             self.mGaitManager.start()  # Gait generator enters walking state  
             self.mGaitManager.setXAmplitude(1.0)  # Set robot forward  
             self.mGaitManager.step(self.mTimeStep)  # Gait generator generates a step action  
             self.myStep()  # Simulate one step  
+            print("GPS:",self.gps.getValues())
+            print("Total Distance traveled in Terms of X Coordinate:",self.gps.getValues()[0]-initialgps[0])
+            #print("GPS Y:",self.gps.getValues()[1]-initialgps[1])
+            print("Total Distance traveled in Terms of Z Coordinate::",self.gps.getValues()[2]-initialgps[2])
+            print("Speed:",self.gps.getSpeed())
 
 
 if __name__ == '__main__':
